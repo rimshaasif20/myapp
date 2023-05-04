0b3e8b126e2128189b48e607165c08f8a8f85e4b
Rimsha <rimshaasif873@gmail.com>
2023-05-04 02:32:18 -0700

    hello

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to web</h1>
+      <h1>Welcome to website</h1>
+      <h2>practice</h2>
+      <p>hello</p>
     </div>
   );
 }
